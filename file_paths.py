5b21ea3cdd24fd99bb97fd64df9bb7e8188b4ab3
Rafael Vera <rafaeldejesusverareyes@gmail.com>
2022-05-29 14:29:08 -0500

    Create file_paths.py

---
+++ file_paths.py
@@ -0,0 +1,16 @@
+# Paths where the generated and processed data are stored
+PATH_CIFAR10_TRAIN_IMAGES = 'datasets/cifar10_train_images.npy'
+PATH_CIFAR10_TRAIN_LABELS = 'datasets/cifar10_train_labels.npy'
+PATH_CIFAR10_TEST_IMAGES  = 'datasets/cifar10_test_images.npy'
+PATH_CIFAR10_TEST_LABELS  = 'datasets/cifar10_test_labels.npy'
+
+# Paths where the augmented data are stored
+PATH_CIFAR10_AUGMENTED_TRAIN_IMAGES = 'datasets/cifar10_augmented_train_images.npy'
+PATH_CIFAR10_AUGMENTED_TRAIN_LABELS = 'datasets/cifar10_augmented_train_labels.npy'
+PATH_CIFAR10_AUGMENTED_TEST_IMAGES  = 'datasets/cifar10_augmented_test_images.npy'
+
+# Paths where the models are stored
+PATH_SHALLOW_MODEL_ADADELTA = 'models/shallow_model_adadelta.h5'
+PATH_DEEP_MODEL_ADADELTA    = 'models/deep_model_adadelta.h5'
+PATH_SHALLOW_MODEL_SGD      = 'models/shallow_model_sgd.h5'
+PATH_DEEP_MODEL_SGD         = 'models/deep_model_sgd.h5'
