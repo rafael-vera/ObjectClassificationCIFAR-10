374825e89423f421762b61a58dc3f655e57423fd
Rafael Vera <rafaeldejesusverareyes@gmail.com>
2022-05-29 14:29:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-from generate_datasets import save_datasets
+from generate_data import generate_data
+from model import DeepLearningModel
+from file_paths import *
 
 def main() -> None:
-    save_datasets(with_augment=False)
-    pass
+    # Uncomment the line below to generate CIFAR-10 data
+    #generate_data(with_augment=False)
+    
+    # The model is instantiated
+    model = DeepLearningModel()
+    
+    # The first model is shallow with Adadelta optimizer
+    model.build_shallow_model('Adadelta')
+    model.train_model()
+    model.save_model(PATH_SHALLOW_MODEL_ADADELTA)
+    
+    # The second model is deep with Adadelta optimizer
+    model.build_deep_model('Adadelta')
+    model.train_model()
+    model.save_model(PATH_DEEP_MODEL_ADADELTA)
+    
+    # The third model is shallow with SGD optimizer
+    model.build_shallow_model('SGD')
+    model.train_model()
+    model.save_model(PATH_SHALLOW_MODEL_SGD)
+    
+    # The fourth model is deep with SGD optimizer
+    model.build_deep_model('SGD')
+    model.train_model()
+    model.save_model(PATH_DEEP_MODEL_SGD)
 
 if __name__ == '__main__':
     main()
